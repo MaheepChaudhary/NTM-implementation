b0aecfbcd9db33d83d65185aa0514168ebf29c4f
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2021-03-08 12:16:55 +0530

    first_commit

--- ntm.py
+++ ntm.py
@@ -3,7 +3,21 @@ import numpy as np
 import tensorflow.keras as tk
 
 from tensorflow.keras import backend as K
-from tensorflow.kera.layers import RNN,Dense 
-from tensorflow.keras.activations import softmax,Relu,tanh
+from tensorflow.keras.layers import RNN,Dense
+from tensorflow.keras.activations import softmax,tanh
+import sklearn
+from sklearn.metrics.pairwise import cosine_similarity
 
+'''
+def _roll_out(leng,n_shifts):
+
+    eye = np.eye(leng)
+    shifts = range(n_shifts//2,-n_shifts//2,-1)
+    C = np.asarray([np.roll(eye,s,axis = 
+'''
+
+def renorm(s):
+    return s/K.sum(s,axis = 1,keepdims = True)
+
+def cosine_similarity(K,M):
 
