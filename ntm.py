d720fe8f17830dd259125d8c2a43d4de071d3cd9
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2021-03-08 10:04:32 +0530

    first_commit

---
+++ ntm.py
@@ -0,0 +1,9 @@
+import tensorflow as tf 
+import numpy as np 
+import tensorflow.keras as tk
+
+from tensorflow.keras import backend as K
+from tensorflow.kera.layers import RNN,Dense 
+from tensorflow.keras.activations import softmax,Relu,tanh
+
+
