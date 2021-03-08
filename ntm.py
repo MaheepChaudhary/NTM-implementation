04867980a10c3dbd0830a32cfdf1e6962642ea1f
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2021-03-08 13:07:45 +0530

    first_commit

--- ntm.py
+++ ntm.py
@@ -9,7 +9,7 @@ import sklearn
 from sklearn.metrics.pairwise import cosine_similarity
 
 '''
-def _roll_out(leng,n_shifts):
+def _roll_out(leng,n_shifts): #not clear
 
     eye = np.eye(leng)
     shifts = range(n_shifts//2,-n_shifts//2,-1)
@@ -20,4 +20,14 @@ def renorm(s):
     return s/K.sum(s,axis = 1,keepdims = True)
 
 def cosine_similarity(K,M):
+    return cosine_similarity(K,M)
+
+def controller_read_ouput_dim(shift_range,input_dim):
+
+    beta,k,g,sharpness = 1,1,1,1
+    shift = shift_range #not clear
+    return (input_dim+k+beta+g+shift+sharpness)
+
+def controller_write_output_dim(input_):
+
 
